f85a6d8f5392eaadd49787dfdc0925916573720e
Oddthegreat <oddthegreatest@gmail.com>
2025-11-03 21:18:47 +0000

    if the live event are not much adjuste the evnts tat der is no empty spa

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -218,7 +218,7 @@ export default function Home() {
                 </Button>
               </div>
 
-              <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+              <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 justify-center">
                 {filteredEvents.map((event) => (
                   <Card
                     key={event.id}
