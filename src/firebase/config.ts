6b0c7293a97684446a3d5d1e9a0f3073978fae83
Oddthegreat <oddthegreatest@gmail.com>
2025-11-04 02:40:40 +0000

    File changes

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,8 +1,8 @@
 export const firebaseConfig = {
-  "projectId": "studio-2938930845-d3c26",
-  "appId": "1:938167735398:web:96e14ef4c03b1378e12154",
-  "apiKey": "AIzaSyC-QYlcbO4S1hJ1TgrBq29P2u194n8n2O0",
-  "authDomain": "studio-2938930845-d3c26.firebaseapp.com",
-  "measurementId": "G-RJN320E905",
-  "storageBucket": "studio-2938930845-d3c26.appspot.com"
+  "projectId": "studio-2938930845-6b0d1",
+  "appId": "1:196804376543:web:1d9a10fc4d1393e842766a",
+  "apiKey": "AIzaSyCWzVpcrV-PDrEA73-kd1tuSUjjGAzcVjU",
+  "authDomain": "studio-2938930845-6b0d1.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "196804376543"
 };
