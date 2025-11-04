0b4951e0c1da0a5b027397bd1d975d8657d8d246
Oddthegreat <oddthegreatest@gmail.com>
2025-11-04 02:02:12 +0000

    createErrorInternal@https://6000-firebase-studio-1762199227866.cluster-l

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,8 +1,8 @@
+
 export const firebaseConfig = {
-  "projectId": "studio-2938930845-6b0d1",
-  "appId": "1:196804376543:web:1d9a10fc4d1393e842766a",
-  "apiKey": "AIzaSyCWzVpcrV-PDrEA73-kd1tuSUjjGAzcVjU",
-  "authDomain": "studio-2938930845-6b0d1.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "196804376543"
+  projectId: 'hype-connect-428615',
+  appId: '1:1037418251914:web:e7c0b387431da764356708',
+  apiKey: 'AIzaSyBPx620YV3Y5l2i-JcZqGq-Y224UAZ4L1s',
+  authDomain: 'hype-connect-428615.firebaseapp.com',
+  storageBucket: 'hype-connect-428615.appspot.com',
 };
