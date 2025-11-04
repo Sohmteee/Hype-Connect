284e28bff871395f04d67da68db8062ac76279f0
Oddthegreat <oddthegreatest@gmail.com>
2025-11-04 01:38:59 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-2938930845-6b0d1",
+  "appId": "1:196804376543:web:1d9a10fc4d1393e842766a",
+  "apiKey": "AIzaSyCWzVpcrV-PDrEA73-kd1tuSUjjGAzcVjU",
+  "authDomain": "studio-2938930845-6b0d1.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "196804376543"
+};
